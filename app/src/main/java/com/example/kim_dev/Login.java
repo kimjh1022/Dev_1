4df1a77deba1be6a305020cd1187841964d249c7
jh kim <kimjhwan1022@naver.com>
2022-07-14 17:18:45 +0900

    commit

--- app/src/main/java/com/example/kim_dev/Login.java
+++ app/src/main/java/com/example/kim_dev/Login.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
 
+    EditText id, pw;
+    CheckBox Login_keep;
     Button login_btn, join_btn;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +34,13 @@ public class Login extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
-        login_btn = findViewById(R.id.login_btn);
-        join_btn = findViewById(R.id.join_btn);
+        id = (EditText)findViewById(R.id.id);
+        pw = (EditText)findViewById(R.id.pw);
+        Login_keep = (CheckBox) findViewById(R.id.Login_keep);
+        login_btn = (Button)findViewById(R.id.login_btn);
+        join_btn = (Button)findViewById(R.id.join_btn);
 
-        // 메인 페이지로 이동
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        firebaseAuth =  FirebaseAuth.getInstance();
 
         // 회원가입 페이지로 이동
         join_btn.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +48,29 @@ public class Login extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), Join.class);
                 startActivity(intent);
+            }
+        });
+
+        // 메인 페이지로 이동
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = id.getText().toString().trim();
+                String pwd = pw.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(Login.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(Login.this, "로그인 오류", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
             }
         });
     }
